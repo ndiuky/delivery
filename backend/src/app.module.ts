b1d933964f78aa5b102f1af67dff670b30feff16
Farid Tipayev <anod100002@gmail.com>
2025-08-23 17:30:21 +0400

    chore: add new db models

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     AuthModule,
+    CategoryModule,
+    ProductModule,
   ],
 })
 export class AppModule {}
